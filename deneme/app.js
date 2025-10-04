a5ac02001778d38c579916d434dd2602a7d2f74e
Karamambah54 <isik.alideniz54@gmail.com>
2025-10-04 23:51:45 +0300

    2.Gelistirme

--- deneme/app.js
+++ deneme/app.js
@@ -1,3 +1,8 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -8,7 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
+console.log("ENV",process.env);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,8 +23,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(()=>{
+app.use((req,res,next)=>{
   console.log("Middleware çalıştı");
+  next();
 });
 
 app.use('/', indexRouter);
